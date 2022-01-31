acb8a07c3f843c0aeb58d730e565b301449dd0cd
Randel Ramirez <randelramirez1@gmail.com>
2022-01-31 22:52:42 +1100

    change folder name and location of shoe images

--- ShoeContainer/src/Services/ProductCatalogApi/Controllers/ImageController.cs
+++ ShoeContainer/src/Services/ProductCatalogApi/Controllers/ImageController.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ProductCatalogApi.Controllers
@@ -18,7 +13,7 @@ namespace ProductCatalogApi.Controllers
         public IActionResult GetImage(int id,[FromServices] IWebHostEnvironment environment)
         {
             var webRooth = environment.WebRootPath;
-            var path = Path.Combine(webRooth, "Pics", $"shoes-{id.ToString()}.png");
+            var path = Path.Combine(webRooth, "images","shoes", $"shoes-{id.ToString()}.png");
             var buffer = System.IO.File.ReadAllBytes(path);
             return File(buffer, "image/png");
         }
