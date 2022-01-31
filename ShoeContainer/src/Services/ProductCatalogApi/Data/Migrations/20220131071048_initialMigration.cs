cabd127cd5a1664898aa7effcfb96c7d1013f0f8
Randel Ramirez <randelramirez1@gmail.com>
2022-01-31 19:28:42 +1100

    Add Database Migration and Setup local EF CLI tool

---
+++ ShoeContainer/src/Services/ProductCatalogApi/Data/Migrations/20220131071048_initialMigration.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductCatalogApi.Data.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "catalog_brand_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "catalog_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "catalog_type_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateTable(
+                name: "CatalogBrand",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogBrand", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Catalog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PictureFileName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PictureUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CatalogTypedId = table.Column<int>(type: "int", nullable: false),
+                    CatalogBrandId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Catalog", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Catalog_CatalogBrand_CatalogBrandId",
+                        column: x => x.CatalogBrandId,
+                        principalTable: "CatalogBrand",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Catalog_CatalogType_CatalogTypedId",
+                        column: x => x.CatalogTypedId,
+                        principalTable: "CatalogType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Catalog_CatalogBrandId",
+                table: "Catalog",
+                column: "CatalogBrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Catalog_CatalogTypedId",
+                table: "Catalog",
+                column: "CatalogTypedId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Catalog");
+
+            migrationBuilder.DropTable(
+                name: "CatalogBrand");
+
+            migrationBuilder.DropTable(
+                name: "CatalogType");
+
+            migrationBuilder.DropSequence(
+                name: "catalog_brand_hilo");
+
+            migrationBuilder.DropSequence(
+                name: "catalog_hilo");
+
+            migrationBuilder.DropSequence(
+                name: "catalog_type_hilo");
+        }
+    }
+}
