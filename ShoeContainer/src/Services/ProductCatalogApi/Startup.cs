cdfe91eeaed9642c9ab6855d8d1b0782f8f8be3e
Randel Ramirez <randelramirez1@gmail.com>
2022-02-01 01:27:40 +1100

    code clean up

--- ShoeContainer/src/Services/ProductCatalogApi/Startup.cs
+++ ShoeContainer/src/Services/ProductCatalogApi/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using ProductCatalogApi.Data;
 
@@ -42,17 +35,13 @@ namespace ProductCatalogApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductCatalogApi v1"));
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
