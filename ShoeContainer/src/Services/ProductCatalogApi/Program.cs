71f2edfd482c2e26c73b7f391a0d5ebbed1b37b2
Randel Ramirez <randelramirez1@gmail.com>
2022-01-31 22:10:44 +1100

    code clean up

--- ShoeContainer/src/Services/ProductCatalogApi/Program.cs
+++ ShoeContainer/src/Services/ProductCatalogApi/Program.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -13,31 +10,35 @@ namespace ProductCatalogApi
 {
     public class Program
     {
-        public  static async Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            // CreateHostBuilder(args).Build().Run();
-
             var host = CreateHostBuilder(args).Build();
 
-            using var scope = host.Services.CreateScope();
-            var services = scope.ServiceProvider;
-            try
+            var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ==
+                                Environments.Development;
+
+            if (isDevelopment)
             {
-                var context = services.GetRequiredService<CatalogContext>();
-                await CatalogSeed.SeedAsync(context);
-            }
-            catch (Exception e)
-            {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(e,"An error occured while seeding the database");
-                Console.WriteLine(e);
-                throw;
+                using var scope = host.Services.CreateScope();
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<CatalogContext>();
+                    await CatalogSeed.SeedAsync(context);
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error occured while seeding the database");
+                    Console.WriteLine(e);
+                    throw;
+                }
             }
 
             await host.RunAsync();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
