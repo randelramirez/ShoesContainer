1cf57c520093e7df317e00855ea0b47e0743214b
Randel Ramirez <randelramirez1@gmail.com>
2022-02-01 18:55:50 +1100

    code clean up

--- ShoeContainer/src/Services/ProductCatalogApi/Startup.cs
+++ ShoeContainer/src/Services/ProductCatalogApi/Startup.cs
@@ -11,36 +11,40 @@ namespace ProductCatalogApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CatalogOptions>(Configuration.GetSection(CatalogOptions.Catalog));
-            services.AddDbContext<CatalogContext>(options => 
+            services.AddDbContext<CatalogContext>(options =>
                 options.UseSqlServer(Configuration["ConnectionString"]));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProductCatalogApi", Version = "v1" });
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "ProductCatalogApi",
+                        Version = "v1"
+                    });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app,
+            IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductCatalogApi v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "ProductCatalogApi v1"));
             }
-
+            
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseAuthorization();
