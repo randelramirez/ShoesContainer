542d9c8d38ffe5a4a1b2d9d1b2c04e55cdc6cdd8
Randel Ramirez <randelramirez1@gmail.com>
2022-02-07 18:14:51 +1100

    update file structure

--- ShoeContainer/TokenServiceApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ShoesOnContainers.Services.TokenServiceApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- ShoeContainer/TokenServiceApi/Models/ManageViewModels/GenerateRecoveryCodesViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ShoesOnContainers.Services.TokenServiceApi.Models.ManageViewModels
-{
-    public class GenerateRecoveryCodesViewModel
-    {
-        public string[] RecoveryCodes { get; set; }
-    }
-}
